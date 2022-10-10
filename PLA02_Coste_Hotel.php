8dd44f6db30328277350735cf673ba8b7dd012ee
Daniel <daniel.migales.puertas@gmail.com>
2022-10-10 13:26:40 +0200

    finalizado

--- PLA02_Coste_Hotel.php
+++ PLA02_Coste_Hotel.php
@@ -1,71 +1,109 @@
 <?php
 
-//print_r($_POST);
-
 //variables
 $numeroNoches = 0;
 $ciudad = null;
 $diasAlquilerCoche = 0;
 
-$error = null;
+//constantes
 const PRECIOXNOCHE = 60;
 const PRECIOXDIA_COCHE = 40;
 const DTO_ALQUILER_COCHE = 20;
 const DTO_ALQUILER_COCHE_2 = 50;
 
+//variables de los precios totales
 $costeTotalHotel = 0;
 $costeTotalAvion = 0;
 $costeTotalCoche = 0;
 $costeTotalViaje = 0;
 
+//array para todos los errores
+$errores = [];
+
 //detecccion del submit
 if (isset($_POST['enviar'])) {
 
+	//NUMERO DE NOCHES
 	try {
-
 		//recuperar numero de noches
 		$numeroNoches = filter_input(INPUT_POST, 'noches', FILTER_VALIDATE_INT);
-
 		//validar que sea mayor de 0
-		if ($numeroNoches <= 0) {
+		if (!is_numeric($numeroNoches) || $numeroNoches <= 0) {
 			throw new Exception("Seleccione el numero de noches. Introduzca un numero mayor que 0.", 1);
 		}
+	} catch (Exception $e) {
+		$errores[] = 'Error nº: ' . $e->getCode() . '. ' . $e->getMessage() . '<br>';
+	}
 
+	//CIUDAD DE DESTINO
+	try {
 		//recuperar ciudad de destino
 		if (!$ciudad = filter_input(INPUT_POST, 'ciudad')) {
 			throw new Exception("Seleccione la ciudad de destino.", 2);
 		}
+	} catch (Exception $e) {
+		$errores[] = 'Error nº: ' . $e->getCode() . '. ' . $e->getMessage() . '<br>';
+	}
 
+	//NUMERO DE ALQUILER DE COCHE
+	try {
 		//recuperar dias alquiler coche
 		$diasAlquilerCoche = filter_input(INPUT_POST, 'coche', FILTER_VALIDATE_INT);
-
 		//validar que sea mayor de 0 y que no este vacio
 		if (!$diasAlquilerCoche == null && $diasAlquilerCoche <= 0) {
 			throw new Exception("Seleccione los dias de alquiler de vehiculo. Introduzca un numero mayor que 0.", 3);
 		}
-
 		//impedir que se ejecute el metodo calculo del coste de alquiler si los dias son 0
 		$costeTotalCoche = $diasAlquilerCoche == null ? 0 : costeCoche($diasAlquilerCoche);
+	} catch (Exception $e) {
+		$errores[] = 'Error nº: ' . $e->getCode() . '. ' . $e->getMessage() . '<br>';
+	}
 
+	//COMPARAR DIAS DE ESTANCIA Y ALQUILER DE COCHE
+	try {
 		//validar que no sea mayor que el numero de estancia
 		if ($diasAlquilerCoche > $numeroNoches) {
 			throw new Exception("Seleccione los dias de alquiler de vehiculo. No debe ser superior al numero de dias de estancia.", 4);
 		}
 	} catch (Exception $e) {
-		$error = 'Error nº: ' . $e->getCode() . '. ' . $e->getMessage();
+		$errores[] = 'Error nº: ' . $e->getCode() . '. ' . $e->getMessage() . '<br>';
+	}
+
+	//SI SE INTRODUCEN CORRECTAMENTE EL NUMERO DE NOCHES, CIUDAD DE DESTINO Y LOS DIAS DE ALQUILER REALIZARA LOS CALCULOS
+	if ($numeroNoches && $ciudad && $diasAlquilerCoche <= $numeroNoches) {
+		//llamada al metodo de calculo de coste noches hotel
+		$costeTotalHotel = costeHotel($numeroNoches);
+		//llamada al metodo coste avion
+		$costeTotalAvion = costeAvion($ciudad);
+		//llamada al metodo de calculo de coste coche
+		$costeTotalCoche = costeCoche($diasAlquilerCoche);
+		//lamada al metodo de calculo total del viaje
+		$costeTotalViaje = calculoTotalPrecioViaje($costeTotalHotel, $costeTotalAvion, $costeTotalCoche);
 	}
 }
 
 //funcion coste hotel
 function costeHotel($numeroNoches)
 {
+	//relanzar excepcion
+	try {
+		$numeroNoches = filter_input(INPUT_POST, 'noches', FILTER_VALIDATE_INT);
+	} catch (Exception $e) {
+		throw new Exception($e->getMessage(), $e->getCode());
+	}
 	return $numeroNoches * PRECIOXNOCHE;
 }
 
 //funcion coste avion
 function costeAvion($ciudad)
 {
-
+	//relanzar excepcion
+	try {
+		$ciudad = filter_input(INPUT_POST, 'ciudad');
+	} catch (Exception $e) {
+		throw new Exception($e->getMessage(), $e->getCode());
+	}
+	//bucle de seleccion de ciudad
 	switch ($ciudad) {
 		case ($ciudad == 'Madrid'):
 			return $costeTotalAvion = 150;
@@ -80,13 +118,19 @@ function costeAvion($ciudad)
 			return $costeTotalAvion = 200;
 			break;
 		default:
-			echo 'ciudad no valida';
 			return $costeTotalAvion = 0;
 	}
 }
 
 function costeCoche($diasAlquilerCoche)
 {
+	//relanzar excepcion
+	try {
+		$diasAlquilerCoche = filter_input(INPUT_POST, 'coche', FILTER_VALIDATE_INT);
+	} catch (Exception $e) {
+		throw new Exception($e->getMessage(), $e->getCode());
+	}
+	//bucle de calculo coste coche aplicando descuento segun los dias
 	if ($diasAlquilerCoche >= 7) {
 		$costeTotalCoche = ($diasAlquilerCoche * PRECIOXDIA_COCHE) - 50;
 	} else if ($diasAlquilerCoche >= 3) {
@@ -100,23 +144,13 @@ function costeCoche($diasAlquilerCoche)
 //funcion calculo total precio
 function calculoTotalPrecioViaje($costeTotalHotel, $costeTotalAvion, $costeTotalCoche)
 {
-	$costeTotalViaje = $costeTotalHotel + $costeTotalAvion + $costeTotalCoche;
+	$costeTotalViaje = 'Hotel:' . $costeTotalHotel . '€,' . ' Avion:' . $costeTotalAvion . '€,' . ' Vehiculo:' . $costeTotalCoche . '€.' . ' PRECIO TOTAL:' . ($costeTotalHotel + $costeTotalAvion + $costeTotalCoche) . '€';
 	return $costeTotalViaje;
 }
 
-//llamada al metodo de calculo de coste noches hotel
-$costeTotalHotel = costeHotel($numeroNoches);
-
-//llamada al metodo coste avion
-$costeTotalAvion = costeAvion($ciudad);
-
-//llamada al metodo de calculo de coste coche
-$costeTotalCoche = costeCoche($diasAlquilerCoche);
-
-//lamada al metodo de calculo total del viaje
-$costeTotalViaje = calculoTotalPrecioViaje($costeTotalHotel, $costeTotalAvion, $costeTotalCoche);
-
 ?>
+
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -173,7 +207,9 @@ $costeTotalViaje = calculoTotalPrecioViaje($costeTotalHotel, $costeTotalAvion, $
 					<input type="text" class="form-control" name="total" id="total" disabled value='<?= $costeTotalViaje ?>'>
 				</div>
 			</div><br>
-			<span class='errores'><?= $error ?></span>
+			<span class='errores'><?php foreach ($errores as $value) {
+										echo $value, "\n";
+									} ?></span>
 		</form>
 	</main>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
